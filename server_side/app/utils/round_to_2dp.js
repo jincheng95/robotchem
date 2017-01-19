bb3c44249b1fb23855c0bf1006c0a4cd86bfb972
Jin Cheng <cheng.jin@me.com>
2017-01-19 16:00:47 +0000

    docs

--- server_side/app/utils/round_to_2dp.js
+++
@@ -1,3 +0,0 @@
-export default function round_to_2dp(n) {
-  return Math.round(n*100) / 100
-}
--- server_side/controls/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ControlsConfig(AppConfig):
-    name = 'controls'
-
